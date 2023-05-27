3330d7a6cdfbc9be5b5eceff4a912865306bfe23
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-05-27 18:42:36 +0700

    beforeIntegrate

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  Drawer,
+  IconButton,
+  useMediaQuery,
+  Box,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import "./Navbar.css";
+import Logo from "../assets/RecycleJourney2.svg";
 
-function Navbar() {
+const Navbar = () => {
+  const isTabletMobile = useMediaQuery("(max-width: 1075px)");
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ backgroundColor: "transparent" }}>
+      {/* <Box
+            component="img"
+            sx={{
+              width: 306,
+              marginTop: "150px",
+              display: { xs: "inline-block", md: "none" },
+            }}
+            alt="Logo"
+            src="../assets/Logo.png"
+          /> */}
+        <Typography
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1, fontWeight: "600" }}
+          id="LogoName"
+        >
+         
+          RecycleJourney
+        </Typography>
+        {isTabletMobile ? (
+          <>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="end"
+              onClick={handleDrawerToggle}
+            >
+              {isDrawerOpen ? <CloseIcon /> : <MenuIcon />}
+            </IconButton>
+            <Drawer
+              anchor="right"
+              open={isDrawerOpen}
+              onClose={handleDrawerToggle}
+              ModalProps={{ keepMounted: true }}
+              PaperProps={{
+                sx: { width: "275px", backgroundColor: "#8FBDD3" },
+              }}
+            >
+              <div
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  p: 2,
+                }}
+              >
+                <Button
+                  id="button1D"
+                  component={Link}
+                  to="/"
+                  color="inherit"
+                  onClick={handleDrawerToggle}
+                  sx={{ width: "100%" }} // Set the width to 100%
+                >
+                  Home
+                </Button>
+                <Button
+                  id="button2D"
+                  component={Link}
+                  to="/cal-events"
+                  color="inherit"
+                  onClick={handleDrawerToggle}
+                  sx={{ width: "100%" }} // Set the width to 100%
+                >
+                  Calculate / Events
+                </Button>
+                <Button
+                  id="button3D"
+                  component={Link}
+                  to="/sign-in"
+                  color="inherit"
+                  onClick={handleDrawerToggle}
+                  sx={{ width: "100%" }} // Set the width to 100%
+                >
+                  Sign In
+                </Button>
+              </div>
+            </Drawer>
+          </>
+        ) : (
+          <div>
+            <Button id="button1" component={Link} to="/" color="inherit">
+              Home
+            </Button>
+            <Button
+              id="button2"
+              component={Link}
+              to="/cal-events"
+              color="inherit"
+            >
+              Calculate / Events
+            </Button>
+            <Button id="button3" component={Link} to="/sign-in" color="inherit">
+              Sign In
+            </Button>
+          </div>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ frontend/src/components/Table.jsx
@@ -0,0 +1,170 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+
+function createData(name, max, min, information) {
+  return {
+    name,
+    max,
+    min,
+    information,
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="right">{row.max}</TableCell>
+        <TableCell align="right">{row.min}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Information
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Type</TableCell>
+                    <TableCell align="right">Price / kg</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.information.map((informationRow) => (
+                    <TableRow key={informationRow.Name}>
+                      <TableCell component="th" scope="row">
+                        {informationRow.Name}
+                      </TableCell>
+                      <TableCell align="right">{informationRow.pricePerKg}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    max: PropTypes.number.isRequired,
+    min: PropTypes.number.isRequired,
+    information: PropTypes.arrayOf(
+      PropTypes.shape({
+        pricePerKg: PropTypes.string.isRequired,
+        Name: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+  }).isRequired,
+};
+
+const rows = [
+  createData('Paper', 2.30, 0.60, [
+    {
+      Name: 'White/Black',
+      pricePerKg: '2.30',
+    },
+    {
+      Name: 'Book',
+      pricePerKg: '0.60',
+    },
+  ]),
+  createData('Steel', 2.80, 5.40, [
+    {
+      Name: 'Metal Sheet',
+      pricePerKg: '2.80',
+    },
+    {
+      Name: 'Wire Rope',
+      pricePerKg: '5.40',
+    },
+  ]),
+  createData('Glass', 0.70, 2.80, [
+    {
+      Name: 'Beer Bottle',
+      pricePerKg: '0.70',
+    },
+    {
+      Name: 'Broken Glass(Green)',
+      pricePerKg: '2.10',
+    },
+    {
+      Name: 'Broken Glass(Clear)',
+      pricePerKg: '2.80',
+    },
+  ]),
+  createData('Plastic', 5.00, 0.50, [
+    {
+      Name: 'PET',
+      pricePerKg: '0.50',
+    },
+    {
+      Name: 'Foam',
+      pricePerKg: '1.50',
+    },
+    {
+      Name: 'plastic PE',
+      pricePerKg: '1.30',
+    },
+    {
+      Name: 'HDPE',
+      pricePerKg: '5.00',
+    },
+  ]),
+];
+
+export default function CollapsibleTable() {
+  return (
+    <TableContainer component={Paper} sx={{ borderRadius: '30px' }}>
+      <Table aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell />
+            <TableCell>Categories</TableCell>
+            <TableCell align="right">Max-price</TableCell>
+            <TableCell align="right">Min-price</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <Row key={row.name} row={row} />
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
