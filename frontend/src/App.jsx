e1f218480670f57c50caf72ae60f399a1f10b67a
tartartar <112954170+itsChewin@users.noreply.github.com>
2023-05-27 22:49:57 +0700

    Login Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,47 @@
-import { useState, useMemo } from 'react'
-import './App.css'
-import { Route, Routes, } from 'react-router-dom'
-import GlobalContext from "../src/share/GlobalContext"
-import Home from './pages/Home'
-import CalEvents from './pages/CalEvents'
+import { useState, useMemo } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import GlobalContext from "../src/share/GlobalContext";
+import Home from "./pages/Home";
+import CalEvents from "./pages/CalEvents";
+import Auth from "./pages/Auth";
+import Profile from "./pages/Profile";
 
 function App() {
-	const [status, setStatus] = useState('');
-	const [user, setUser] = useState(false);
-	const generatekey = () => {
-		return Math.random();
-	};
+  const [status, setStatus] = useState("");
+  const [user, setUser] = useState(false);
+  const generatekey = () => {
+    return Math.random();
+  };
 
+  const globalContextValue = useMemo(() => {
+    return {
+      user,
+      status,
+      setUser,
+      setStatus,
+    };
+  }, [user]);
 
-	const globalContextValue = useMemo(() => {
-		return {
-			user,
-			status,
-			setUser,
-			setStatus,
-		};
-	}, [user]);
+  return (
+    <GlobalContext.Provider value={globalContextValue}>
+      <Routes>
+        <Route exect path="/" element={<Home />} />
+        <Route path="/cal-events" element={<CalEvents />} />
+        <Route path="/auth" element={<Auth />} />
+		<Route path="/profile" element={<Profile />} />
+      </Routes>
 
-
-	return (
-		<GlobalContext.Provider value={globalContextValue}>
-			<Routes>
-				<Route exect path="/" element={<Home />} />
-				<Route path="/cal-events" element={<CalEvents />} />
-			</Routes>
-
-			{status ? (
-				<SnackBarMessage key={generatekey()} open={status.open} severity={status.severity} message={status.msg} />
-			) : null}
-		</GlobalContext.Provider >
-	)
+      {status ? (
+        <SnackBarMessage
+          key={generatekey()}
+          open={status.open}
+          severity={status.severity}
+          message={status.msg}
+        />
+      ) : null}
+    </GlobalContext.Provider>
+  );
 }
 
-export default App
+export default App;
