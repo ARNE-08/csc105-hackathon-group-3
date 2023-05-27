55c56f47f6c5a3186601686de9aefdf95425195b
Paramita Saenghao <paramita.saen@mail.kmutt.ac.th>
2023-05-28 04:13:26 +0700

    integrate profile user

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,246 +1,320 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import { Box, Grid, Typography, Button } from "@mui/material";
-import imageSrc from "../assets/profilepic1.jpg";
+// import imageSrc from "../assets/profilepic1.jpg";
 import CardProfile from "../components/CardProfile";
 import "./Profile.css";
+import GlobalContext from "../share/GlobalContext";
+
+import { AxiosError } from "axios";
+import Axios from "../share/AxiosInstance";
 
 function Profile() {
+  const { user, setUser } = useContext(GlobalContext)
+  const { status, setStatus } = useContext(GlobalContext)
+
+  const [profileUser, setProfileUser] = useState([])
+
+  useEffect(() => {
+    Axios.get("/profile")
+      .then((response) => {
+        const responseData = response.data;
+        console.log(response.data)
+        if (responseData.success) {
+          setProfileUser(responseData.data);
+          console.log(responseData.data)
+        }
+      })
+      .catch((error) => {
+        if (error instanceof AxiosError) {
+          if (error.response) {
+            return setStatus({
+              msg: error.response.data.error,
+              severity: 'error',
+            });
+          }
+        }
+        return setStatus({
+          msg: error.message,
+          severity: 'error',
+        });
+      });
+  }, []);
+
   return (
-    <>
-    <Box sx={{backgroundColor: '#8FBDD3'}}>
-      <Box sx={{ backgroundColor: "#2C2639" }}>
-        <Navbar />
-      </Box>
+    <div class="profileBG">
+      <Box sx={{ backgroundColor: '#8FBDD3' }}>
+        <Box sx={{ backgroundColor: "#2C2639" }}>
+          <Navbar />
+        </Box>
 
-      <Grid container>
-        <Grid item xs={12} sm={4}>
-          <Box sx={{ height: "100vh" }}>
+        <Grid container>
+          <Grid item xs={12} sm={4}>
+            <div className="UserInform">
+              <Box sx={{ height: "100vh" }}>
+                {/* <Box
+                sx={{
+                  position: "relative",
+                  backgroundImage: {profileUser.profile_picture},
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  height: "40vh",
+                  width: "40vh",
+                  borderRadius: "50%",
+                  margin: "auto",
+                  marginTop: "70px",
+                }}
+              ></Box> */}
+                <div className="ProfileBorder">
+                  <Box
+                    class="ProfilePagePicture"
+                    component='img'
+                    alt="Logo"
+                    src={profileUser.profile_picture}
+                  />
+                </div>
+
+                <div className="UserInformBox">
+                  <Box sx={{ margin: "20px" }}>
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="Hometext title"
+                      margin={"10px"}
+                      color={'white'}
+                      sx={{ fontFamily: "'Poppins', sans-serif" }}
+                    >
+                      fullname :
+                      <Typography
+                        sx={{ fontFamily: "'Poppins', sans-serif" }}
+                      >{profileUser.fullname}</Typography>
+                      {/* <input
+                    type="text"
+                    style={{
+                      border: "none",
+                      borderBottom: "2px solid white",
+                      width: "100%",
+                      height: "30px",
+                      fontSize: "20px",
+                      fontWeight: "600",
+                      color: "black",
+                      outline: "none",
+                      marginBottom: "10px",
+                      backgroundColor: "#8FBDD3",
+                    }}
+                  /> */}
+                    </Typography>
+
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="Hometext title"
+                      margin={"10px"}
+                      color={'white'}
+                      sx={{ fontFamily: "'Poppins', sans-serif" }}
+                    >
+                      lastname :
+                      <Typography
+                        sx={{ fontFamily: "'Poppins', sans-serif" }}
+                      >{profileUser.lastname}</Typography>
+
+                      {/* <input
+                    type="text"
+                    style={{
+                      border: "none",
+                      borderBottom: "2px solid white",
+                      width: "100%",
+                      height: "30px",
+                      fontSize: "20px",
+                      fontWeight: "600",
+                      color: "black",
+                      outline: "none",
+                      marginBottom: "10px",
+                      backgroundColor: "#8FBDD3",
+                    }}
+                  /> */}
+                    </Typography>
+
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="Hometext title"
+                      margin={"10px"}
+                      color={'white'}
+                      sx={{ fontFamily: "'Poppins', sans-serif" }}
+                    >
+                      company :
+                      <Typography
+                        sx={{ fontFamily: "'Poppins', sans-serif" }}
+                      >
+                        {profileUser.company_organization}</Typography>
+
+                      {/* <input
+                    type="text"
+                    style={{
+                      border: "none",
+                      borderBottom: "2px solid white",
+                      width: "100%",
+                      height: "30px",
+                      fontSize: "20px",
+                      fontWeight: "600",
+                      color: "black",
+                      outline: "none",
+                      marginBottom: "10px",
+                      backgroundColor: "#8FBDD3",
+                    }}
+                  /> */}
+                    </Typography>
+
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="Hometext title"
+                      margin={"10px"}
+                      color={'white'}
+                      sx={{ fontFamily: "'Poppins', sans-serif" }}
+                    >
+                      email :
+                      <Typography
+                        sx={{ fontFamily: "'Poppins', sans-serif" }}
+                      >{profileUser.email}</Typography>
+
+                      {/* <input
+                    type="text"
+                    style={{
+                      border: "none",
+                      borderBottom: "2px solid white",
+                      width: "100%",
+                      height: "30px",
+                      fontSize: "20px",
+                      fontWeight: "600",
+                      color: "black",
+                      outline: "none",
+                      marginBottom: "10px",
+                      backgroundColor: "#8FBDD3",
+                    }}
+                  /> */}
+                    </Typography>
+
+                    <Typography
+                      variant="h6"
+                      component="div"
+                      className="Hometext title"
+                      margin={"10px"}
+                      color={'white'}
+                      sx={{ fontFamily: "'Poppins', sans-serif" }}
+
+                    >
+                      tel :
+                      <Typography
+                        sx={{ fontFamily: "'Poppins', sans-serif" }}
+                      >{profileUser.tel}</Typography>
+
+                      {/* <input
+                    type="text"
+                    style={{
+                      border: "none",
+                      borderBottom: "2px solid white",
+                      width: "100%",
+                      height: "30px",
+                      fontSize: "20px",
+                      fontWeight: "600",
+                      color: "black",
+                      outline: "none",
+                      marginBottom: "10px",
+                      backgroundColor: "#8FBDD3",
+                    }}
+                  /> */}
+                    </Typography>
+                  </Box>
+                </div>
+              </Box>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={8}>
             <Box
               sx={{
-                position: "relative",
-                backgroundImage: `url(${imageSrc})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                height: "40vh",
-                width: "40vh",
-                borderRadius: "50%",
+
+                width: "90%",
+                height: "60vh",
                 margin: "auto",
-                marginTop: "70px",
+                marginTop: "30px",
+                borderRadius: "20px",
+                position: "relative",
               }}
-            ></Box>
-            <Box sx={{ margin: "20px" }}>
-              <Typography
-                variant="h6"
-                component="div"
-                className="Hometext title"
-                margin={"10px"}
-                color={'white'}
-              >
-                fullname :
-                <input
-                  type="text"
-                  style={{
-                    border: "none",
-                    borderBottom: "2px solid white",
-                    width: "100%",
-                    height: "30px",
-                    fontSize: "20px",
-                    fontWeight: "600",
-                    color: "black",
-                    outline: "none",
-                    marginBottom: "10px",
-                    backgroundColor: "#8FBDD3",
-                  }}
-                />
-              </Typography>
+            >
+              <Box sx={{ padding: "30px" }}>
+                <CardProfile />
+              </Box>
 
-              <Typography
-                variant="h6"
-                component="div"
-                className="Hometext title"
-                margin={"10px"}
-                color={'white'}
+              <Box
+                sx={{
+                  position: "absolute",
+                  bottom: "10px",
+                  right: "10px",
+                }}
               >
-                lastname :
-                <input
-                  type="text"
-                  style={{
-                    border: "none",
-                    borderBottom: "2px solid white",
-                    width: "100%",
-                    height: "30px",
-                    fontSize: "20px",
-                    fontWeight: "600",
-                    color: "black",
-                    outline: "none",
-                    marginBottom: "10px",
-                    backgroundColor: "#8FBDD3",
-                  }}
-                />
-              </Typography>
-
-              <Typography
-                variant="h6"
-                component="div"
-                className="Hometext title"
-                margin={"10px"}
-                color={'white'}
-              >
-                company :
-                <input
-                  type="text"
-                  style={{
-                    border: "none",
-                    borderBottom: "2px solid white",
-                    width: "100%",
-                    height: "30px",
-                    fontSize: "20px",
-                    fontWeight: "600",
-                    color: "black",
-                    outline: "none",
-                    marginBottom: "10px",
-                    backgroundColor: "#8FBDD3",
-                  }}
-                />
-              </Typography>
-
-              <Typography
-                variant="h6"
-                component="div"
-                className="Hometext title"
-                margin={"10px"}
-                color={'white'}
-              >
-                email :
-                <input
-                  type="text"
-                  style={{
-                    border: "none",
-                    borderBottom: "2px solid white",
-                    width: "100%",
-                    height: "30px",
-                    fontSize: "20px",
-                    fontWeight: "600",
-                    color: "black",
-                    outline: "none",
-                    marginBottom: "10px",
-                    backgroundColor: "#8FBDD3",
-                  }}
-                />
-              </Typography>
-
-              <Typography
-                variant="h6"
-                component="div"
-                className="Hometext title"
-                margin={"10px"}
-                color={'white'}
-              >
-                tel :
-                <input
-                  type="text"
-                  style={{
-                    border: "none",
-                    borderBottom: "2px solid white",
-                    width: "100%",
-                    height: "30px",
-                    fontSize: "20px",
-                    fontWeight: "600",
-                    color: "black",
-                    outline: "none",
-                    marginBottom: "10px",
-                    backgroundColor: "#8FBDD3",
-                  }}
-                />
-              </Typography>
-            </Box>
-          </Box>
-        </Grid>
-
-        <Grid item xs={12} sm={8}>
-          <Box
-            sx={{
-              
-              width: "90%",
-              height: "60vh",
-              margin: "auto",
-              marginTop: "30px",
-              borderRadius: "20px",
-              position: "relative",
-            }}
-          >
-            <Box sx={{ padding: "30px" }}>
-              <CardProfile />
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  sx={{ backgroundColor: "white", right: "10px" }}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  sx={{ backgroundColor: "white" }}
+                >
+                  Delete
+                </Button>
+              </Box>
             </Box>
 
             <Box
               sx={{
-                position: "absolute",
-                bottom: "10px",
-                right: "10px",
+
+                width: "90%",
+                height: "60vh",
+                margin: "auto",
+                marginTop: "30px",
+                marginBottom: "50px",
+                borderRadius: "20px",
+                position: "relative",
               }}
             >
-              <Button
-                variant="outlined"
-                color="primary"
-                sx={{ backgroundColor: "white", right: "10px" }}
-              >
-                Edit
-              </Button>
-              <Button
-                variant="outlined"
-                color="secondary"
-                sx={{ backgroundColor: "white" }}
-              >
-                Delete
-              </Button>
-            </Box>
-          </Box>
+              <Box sx={{ padding: "30px" }}>
+                <CardProfile />
+              </Box>
 
-          <Box
-            sx={{
-              
-              width: "90%",
-              height: "60vh",
-              margin: "auto",
-              marginTop: "30px",
-              marginBottom: "50px",
-              borderRadius: "20px",
-              position: "relative",
-            }}
-          >
-            <Box sx={{ padding: "30px" }}>
-              <CardProfile />
-            </Box>
-
-            <Box
-              sx={{
-                position: "absolute",
-                bottom: "10px",
-                right: "10px",
-              }}
-            >
-              <Button
-                variant="outlined"
-                color="primary"
-                sx={{ backgroundColor: "white", right: "10px" }}
+              <Box
+                sx={{
+                  position: "absolute",
+                  bottom: "10px",
+                  right: "10px",
+                }}
               >
-                Edit
-              </Button>
-              <Button
-                variant="outlined"
-                color="secondary"
-                sx={{ backgroundColor: "white" }}
-              >
-                Delete
-              </Button>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  sx={{ backgroundColor: "white", right: "10px" }}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  sx={{ backgroundColor: "white" }}
+                >
+                  Delete
+                </Button>
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
       </Box>
-    </>
+    </div>
   );
 }
 
